f3e91a6728b153b357f968833dbd07724f2c3d44
Sonali Mahato <69892681+sonalimahato12@users.noreply.github.com>
2021-07-16 22:47:17 +0530

    Add files via upload

---
+++ sum of  non prime digits.c
@@ -0,0 +1,31 @@
+//sum of non prime digits
+
+#include<stdio.h>
+int main()
+{
+	long int no;
+	int i,sum=0,rem,count=0;
+	
+	scanf("%ld",&no);
+	
+	while(no!=0)
+	{
+		count=0;
+		rem=no%10;
+		for(i=1;i<=rem;i++)
+		{
+			if(rem%i==0)
+			{
+				count++;
+			}
+			
+		}
+		if(count>2)
+		{
+			sum=sum+rem;
+		}
+		no=no/10;
+	}
+	printf("%d",sum);
+	
+}
